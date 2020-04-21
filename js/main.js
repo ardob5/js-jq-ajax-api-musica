f89bc90b249195d3a8b17ef91a9e2e9875a775a2
Riccardo Belli <riccardo_belli@icloud.com>
2020-04-21 02:15:02 +0200

    Completed

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+    $.ajax({
+        url : "https://flynn.boolean.careers/exercises/api/array/music",
+        method : 'GET',
+        success : function(risultato) {
+
+            var source = $("#entry-template").html();
+
+            var template = Handlebars.compile(source);
+
+            var listaAlbum = risultato.response;
+
+            for(var i = 0; i < listaAlbum.length; i++) {
+
+                var albumCorrente = listaAlbum[i];
+                var context = {
+                    poster: albumCorrente.poster,
+                    title : albumCorrente.title,
+                    author : albumCorrente.author,
+                    year : albumCorrente.year,
+                    genre : albumCorrente.genre
+                };
+
+                var risultatoDaAggiungere = template(context);
+
+                $(".cds-container").append(risultatoDaAggiungere);
+            }
+
+        },
+        error : function() {
+
+        }
+    });
+
+    $(".filtro-album select").on("input", function() {
+
+        var filtro = $(this).val().toLowerCase();
+        $(".cd").each(function() {
+
+            var genereCd = $(this).data("genre").toLowerCase();
+            if (filtro === "" || genereCd === filtro) {
+
+                $(this).show();
+            } else {
+
+                $(this).hide();
+            }
+        });
+    });
+});
